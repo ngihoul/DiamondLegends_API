8fcdfd1d976d517148797ba726db452be4713541
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-15 11:14:34 +0100

    Adding middleware to handling errors

--- DiamondLegends.API/Program.cs
+++ DiamondLegends.API/Program.cs
@@ -1,3 +1,7 @@
+using DiamondLegends.BLL.Interfaces;
+using DiamondLegends.BLL.Services;
+using DiamondLegends.DAL.Interfaces;
+using DiamondLegends.DAL.Repositories;
 using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<SqlConnection>(c => new SqlConnection(builder.Configuration.GetConnectionString("Default")));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +30,36 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// TODO : Outsource in separate file
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next.Invoke();
+    }
+    catch (ArgumentNullException e)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsJsonAsync(e.Message);
+    }
+    catch (UnauthorizedAccessException e)
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsJsonAsync(e.Message);
+    }
+    // TODO : Catch error 403
+    catch (ArgumentException e)
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsJsonAsync(e.Message);
+    }
+    catch (Exception e)
+    {
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsJsonAsync(e.Message);
+    }
+});
 
 app.MapControllers();
 
