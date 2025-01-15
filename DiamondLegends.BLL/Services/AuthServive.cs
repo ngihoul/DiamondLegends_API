f61d9e8cc88842d374c7f46cbd5be2b0bed7cf26
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-15 11:24:05 +0100

    Creating AuthService

---
+++ DiamondLegends.BLL/Services/AuthServive.cs
@@ -0,0 +1,64 @@
+﻿
+using DiamondLegends.Domain.Models;
+using Isopoh.Cryptography.Argon2;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CheckMate.BLL.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _config;
+
+        public AuthService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateSalt()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public string HashPassword(string password, string salt)
+        {
+            return Argon2.Hash(salt + password + _config["Pepper"]);
+        }
+
+        public bool Verify(User user, string password)
+        {
+            return Argon2.Verify(user.Password, user.Salt + password + _config["Pepper"]);
+        }
+
+        /*public string GenerateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username ?? ""),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.Name),
+                new Claim("Username", user.Username ?? ""),
+                new Claim("Id", user.Id.ToString()),
+                new Claim("Role", user.Role.Name),
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }*/
+    }
+}
