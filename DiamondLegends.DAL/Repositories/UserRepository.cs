ea7657e1d501e7d5a2ef11b5932135ddbee68fe6
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-15 10:28:49 +0100

    Creating Create function in UserRepository

---
+++ DiamondLegends.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using DiamondLegends.DAL.Interfaces;
+using DiamondLegends.Domain.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DiamondLegends.DAL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public UserRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<User> Create(User user)
+        {
+            await _connection.OpenAsync();
+
+            User newUser = _connection.QuerySingle<User>(
+                "INSERT INTO Users(Username, Email, Password, Salt, Nationality) " +
+                "OUTPUT INSERTED.* " +
+                "VALUES (@Username, @Email, @Password, @Salt, @Nationality)", 
+                user
+            );
+
+            await _connection.CloseAsync();
+
+            return newUser;
+        }
+    }
+}
