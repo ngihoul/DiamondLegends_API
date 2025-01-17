821d36b0279ecd4184b27a85999949a113466204
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-17 13:36:01 +0100

    Team logo not required

--- DiamondLegends.Domain/Models/Team.cs
+++ DiamondLegends.Domain/Models/Team.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string City { get; set; }
         public Country Country { get; set; }
-        public string Logo { get; set; }
+        public string? Logo { get; set; }
         public string Color_1 { get; set; }
         public string Color_2 { get; set; }
         public string Color_3 { get; set; }
