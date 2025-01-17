e3971abd34f1ca12fd28799d858934508c83c938
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-17 15:10:07 +0100

    Restructuring Team entity

--- DiamondLegends.Domain/Models/Team.cs
+++ DiamondLegends.Domain/Models/Team.cs
@@ -1,4 +1,6 @@
-﻿namespace DiamondLegends.Domain.Models
+﻿using System.Numerics;
+
+namespace DiamondLegends.Domain.Models
 {
     public class Team
     {
@@ -6,6 +8,10 @@
         public string Name { get; set; }
         public string City { get; set; }
         public Country Country { get; set; }
+        public League League { get; set; }
+        public int Season { get; set; }
+        public string CurrentDay { get; set; }
+        public BigInteger Budget { get; set; }
         public string? Logo { get; set; }
         public string Color_1 { get; set; }
         public string Color_2 { get; set; }
