8920f195ee9bfa1129faa486d6f5a9d61d3a3b86
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-17 13:51:22 +0100

    Creating Service to Get a team

---
+++ DiamondLegends.BLL/Interfaces/ITeamService.cs
@@ -0,0 +1,11 @@
+﻿using DiamondLegends.Domain.Models;
+
+namespace DiamondLegends.BLL.Interfaces
+{
+    public interface ITeamService
+    {
+        public Task<Team> Get(int id);
+        public Task<IEnumerable<Team>> GetAllByUser(int userId);
+        public Task<Team> Create(Team team);
+    }
+}
---
+++ DiamondLegends.BLL/Services/TeamService.cs
@@ -0,0 +1,40 @@
+﻿using DiamondLegends.BLL.Interfaces;
+using DiamondLegends.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondLegends.BLL.Services
+{
+    public class TeamService : ITeamService
+    {
+        public Task<Team> Create(Team team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Team> Get(int id)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentNullException("Cette équipe n'existe pas");
+            }
+
+            Team? team = await _teamRepository.GetById(id);
+
+            if(team is null)
+            {
+                throw new ArgumentException("Cette équipe n'existe pas");
+            }
+
+            return team;
+        }
+
+        public Task<IEnumerable<Team>> GetAllByUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
