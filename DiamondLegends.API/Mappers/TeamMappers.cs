8952cc6154b3f0b43336fd342c3b0bc110125680
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-17 13:44:13 +0100

    Creating Team Mappers

---
+++ DiamondLegends.API/Mappers/TeamMappers.cs
@@ -0,0 +1,35 @@
+﻿using DiamondLegends.API.DTO;
+using DiamondLegends.Domain.Models;
+
+namespace DiamondLegends.API.Mappers
+{
+    public static class TeamMappers
+    {
+        public static TeamView ToView(this Team team)
+        {
+            return new TeamView()
+            {
+                Id = team.Id,
+                Name = team.Name,
+                City = team.City,
+                Country = team.Country.Name,
+                Logo = team.Logo,
+                Color_1 = team.Color_1,
+                Color_2 = team.Color_2,
+                Color_3 = team.Color_3
+                // public IEnumerable<Player> Players { get; set; }
+            };
+        }
+
+        public static TeamViewList ToViewList(this Team team)
+        {
+            return new TeamViewList()
+            {
+                Id = team.Id,
+                Name = team.Name,
+                Country = team.Country.Name,
+                Logo = team.Logo,
+            };
+        }
+    }
+}
