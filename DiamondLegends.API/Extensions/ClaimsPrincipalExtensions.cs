9d5129cf7274686322da985cdaab673602727b94
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-18 17:12:01 +0100

    Creating Extension to get userId

---
+++ DiamondLegends.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+
+namespace DiamondLegends.API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            string? userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return int.TryParse(userIdClaim, out int userId) ? userId : 0;
+        }
+
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Name)?.Value ?? string.Empty;
+        }
+
+        public static string GetEmail(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Email)?.Value ?? string.Empty;
+        }
+    }
+}
