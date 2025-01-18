755f8ada62ed4c071bee43ede112e4c540eab837
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-18 17:59:57 +0100

    Creating Player entity

---
+++ DiamondLegends.DB/Tables/Players.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [dbo].[Players]
+(
+	[Id] INT IDENTITY,
+	Firstname NVARCHAR(120) NOT NULL,
+	Lastname NVARCHAR(120) NOT NULL,
+	Date_of_birth DATE NOT NULL,
+	Nationality INT NOT NULL,
+	[Throw] INT NOT NULL,
+	Bat INT NOT NULL,
+	Salary DECIMAL NOT NULL,
+	Energy INT NOT NULL DEFAULT 100,
+	Contact INT NOT NULL,
+	Contact_Potential INT NOT NULL,
+	[Power] INT NOT NULL,
+	Power_Potential INT NOT NULL,
+	Running INT NOT NULL,
+	Running_Potential INT NOT NULL,
+	Defense INT NOT NULL,
+	Defense_Potential INT NOT NULL,
+	Mental INT NOT NULL,
+	Mental_Potential INT NOT NULL,
+	Stamina INT NOT NULL,
+	Stamina_Potential INT NOT NULL,
+	[Control] INT NOT NULL,
+	Control_Potential INT NOT NULL,
+	Velocity INT NOT NULL,
+	Velocity_Potential INT NOT NULL,
+	Movement INT NOT NULL,
+	Movement_Potential INT NOT NULL
+
+	CONSTRAINT PK_Players PRIMARY KEY (Id),
+	CONSTRAINT FK_Players_Countries_Nationality FOREIGN KEY (Nationality) REFERENCES Countries (Id)
+)
---
+++ DiamondLegends.Domain/Models/Player.cs
@@ -0,0 +1,34 @@
+﻿namespace DiamondLegends.Domain.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Country Nationality { get; set; }
+        public int Throw { get; set; }
+        public int Bat { get; set; }
+        public int Salary { get; set; }
+        public int Energy { get; set; } = 100;
+        public int Contact { get; set; }
+        public int ContactPotential { get; set; }
+        public int Power { get; set; }
+        public int PowerPotential { get; set; }
+        public int Running { get; set; }
+        public int RunningPotential { get; set; }
+        public int Defense { get; set; }
+        public int DefensePotential { get; set; }
+        public int Mental { get; set; }
+        public int MentalPotential { get; set; }
+        public int Stamina { get; set; }
+        public int StaminaPotential { get; set; }
+        public int Control { get; set; }
+        public int ControlPotential { get; set; }
+        public int Velocity { get; set; }
+        public int VelocityPotential { get; set; }
+        public int Movement { get; set; }
+        public int MovementPotential { get; set; }
+        public IEnumerable<Position> Positions { get; set; }
+    }
+}
