7a0654ec7416c3fd11bd2a1ca3bcc5f73bf2979e
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-27 09:34:09 +0100

    Securizing & documenting Game endpoints

--- DiamondLegends.API/Controllers/GameController.cs
+++ DiamondLegends.API/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using DiamondLegends.API.Mappers;
 using DiamondLegends.BLL.Interfaces;
 using DiamondLegends.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,11 @@ namespace DiamondLegends.API.Controllers
         }
 
         [HttpGet]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<List<Game>>> GetAll([FromQuery] GameQuery query)
         {
             List<Game> games = await _gameService.GetAll(query);
